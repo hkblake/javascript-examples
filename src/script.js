e003edce915dd62d1777bfc7104ce56e45b6a015
hkblake <87841630+hkblake@users.noreply.github.com>
2021-07-28 21:53:36 -0500

    add functions

--- src/script.js
+++ src/script.js
@@ -1,11 +1,32 @@
-function printVariable(variable) {
-  console.log(variable);
-}
-
 let name = "Heather";
 
+// our print name function
+function printName(name) {
+  console.log(name);
+}
+
+// our function that takes a function as a parameter
 function func(name, callback) {
   callback("Hello " + name);
 }
 
-func(name, printVariable);
+// call func and pass the printName function
+func(name, printName);
+
+// call func and pass a function definition
+func("function definition", function (variable) {
+  console.log(variable);
+});
+
+// call func and pass an arrow function
+func("ArrowFunction", (variable) => {
+  console.log(variable);
+});
+
+// set the arrow function to a variable
+// we can put the arrow function on oneline
+// because it is a one line function
+let arrowFunction = (name) => console.log(name);
+
+// call func with arrowFunction variable
+func("ArrowFunctionVariable", arrowFunction);
